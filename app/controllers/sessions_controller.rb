67c7401e03709396c6455bb4f423bd55de503cec
nagata091 <nagata.kz091@gmail.com>
2023-08-08 04:04:02 +0000

    :wip: 第8章途中

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # 入力されたメールアドレスからユーザーを検索して取り出す
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    # ユーザーが存在し、かつパスワードが一致した場合にtrue
+    if user && user.authenticate(params[:session][:password])
+      # セキュリティのため(セッション固定攻撃)、ログイン直前にセッションをリセット
+      reset_session
+      # ヘルパーで作成したlog_inメソッドでログイン
+      log_in user
+      # ユーザー詳細ページにリダイレクト。次と同じこと`user_url(user)`
+      redirect_to user
+    else
+      flash.now[:danger] = "メールアドレスもしくはパスワードがちがうよ！"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+
+  # 渡されたユーザーでログインするメソッド
+  def log_in(user)
+    # sessionメソッドを使うと、ユーザーブラウザ内の一時cookiesに暗号化済のユーザーIDが自動で作成される。
+    # ブラウザを閉じたときに有効期限が終了する。
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返すメソッド
+  def current_user
+    if session[:user_id]
+      # current_userがすでに存在すればそのまま返し、なければセッション中のIDでユーザーを検索して返す
+      @current_user = @current_user || User.find_by(id: session[:user_id])
+    end
+  end
+
+  # ユーザーがログイン済ならtrue、その他ならfalseを返すメソッド
+  def logged_in?
+    # current_userがnilなら(true、つまりログインしてない)falseを返し、nilでないなら(false、つまりログインしている)trueを返す
+    !current_user.nil?
+  end
 end
